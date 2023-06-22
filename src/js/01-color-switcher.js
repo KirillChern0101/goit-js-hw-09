b0c1ba65e7fa92cc8cf0fc1aaaed61be7ac537ac
KirillChern <kirillchernliko@gmail.com>
2023-06-22 22:14:22 +0300

    hw1+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonOn = document.querySelector('[data-start]');
+const buttonOff = document.querySelector('[data-stop]');
 
+let interval = null;
+
+buttonOn.addEventListener('click', startColorChange);
+buttonOff.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  buttonOn.disabled = true;
+  interval = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorChange() {
+  buttonOn.disabled = false;
+  clearInterval(interval);
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
