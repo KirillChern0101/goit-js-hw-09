04d737e6dfb83f386842ef24d0ab1487afa29690
KirillChern0101 <123367766+KirillChern0101@users.noreply.github.com>
2023-06-25 09:27:56 +0000

    Deploying to gh-pages from @ KirillChern0101/goit-js-hw-09@3c5ab98e70d8ca61998006dbd20d5c8d7f8ac533 🚀

--- 01-color-switcher.965c99f4.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));document.body.style.backgroundColor=t}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.965c99f4.js.map
